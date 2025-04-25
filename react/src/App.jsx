91b21106d3a7e290c23a29f48023726546ad1c0c
Ioannis Kollias <johndday07@gmail.com>
2025-04-25 18:53:33 +0300

    State to Save Food Data

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from 'react'
 import Search from './components/Search';
 
 function App() {
+  // we save this state, so can be accessed from the hole application
+  const [foodData, setFoodData] = useState([])
   return (
     <div className='App'>
-      <Search />
+      <Search foodData={foodData} setFoodData={setFoodData}/>
+      {foodData.map((food) => (
+        <h1>{food.title}</h1>
+      ))}
     </div>
   );
 }
--- react/src/components/Search.jsx
+++ react/src/components/Search.jsx
@@ -3,8 +3,8 @@ import { useEffect, useState } from "react";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "3fdced503d074d23ada3dbc7e08ff4b1";
 
-export default function Search() {
-    const [query, setQuery] = useState("pasta");
+export default function Search({ foodData, setFoodData }) {
+    const [query, setQuery] = useState("pizza");
     // syntax of the useEffect hook
     // useEffect(() => {}, []);
     useEffect(() => {
@@ -12,6 +12,7 @@ export default function Search() {
             const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
             const data = await res.json();
             console.log(data.results);
+            setFoodData(data.results);
         }
         fetchFood();
     }, [query]);
